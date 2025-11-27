222b9daa3b6f8e9e9c6914f297b4c1d8a7ae5320
Charmi Venkata Nagalakshmi <aravapallicharmi0@gmail.com>
2025-11-27 20:51:17 +0530

    Add files via upload

---
+++ Arithmetic Operator.py
@@ -0,0 +1,12 @@
+a=int(input())
+b=int(input())
+division=b//a  #a//b
+modular=b%a #a%b
+exponentiation=b**a #a**b
+c=2<<2
+d=3>>3
+e=2>>1
+print(e)
+print(c)
+print(d)
+print( division,'\n',modular,'\n',exponentiation)
