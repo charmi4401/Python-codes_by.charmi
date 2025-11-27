decc355f17b022ebf7d6a094b409f33b9454c8be
Charmi Venkata Nagalakshmi <aravapallicharmi0@gmail.com>
2025-11-27 20:11:52 +0530

    Add examples of string slicing in Python

--- string_slicing.py
+++ string_slicing.py
@@ -1,1 +1,13 @@
-
+a='charmi'
+print(len(a))
+print(a[1])
+print(a[-10])
+print(a[1::2])
+print(a[1:3])
+print(a[1:-1])
+print(a[:3])
+print(a[2:])
+print(a[:-1])
+print(a[::3])
+print(a[1::2])
+print(a[::-1])#print a reverse
