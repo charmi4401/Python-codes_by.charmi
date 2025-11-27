2c3bdc6df28306ab5d95ce7eaee4f3f7242636cf
Charmi Venkata Nagalakshmi <aravapallicharmi0@gmail.com>
2025-11-27 20:53:41 +0530

    Add files via upload

---
+++ String Methods.py
@@ -0,0 +1,8 @@
+a='  charmi, manasa  '
+print(a.upper())
+print(a.lower())
+print(a.capitalize())
+print(a.title())
+print(a.strip())#.lstrip,.rstrip
+print(a.startswith('charmi'))#where the charmi is from starting it print true else it print false
+print(a.replace('a','b'))
