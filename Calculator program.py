9bee086606bb2611f2fde4a0a2055693a152c6d0
Charmi Venkata Nagalakshmi <aravapallicharmi0@gmail.com>
2025-11-27 20:51:44 +0530

    Add files via upload

---
+++ Calculator program.py
@@ -0,0 +1,14 @@
+#Simple Claculator Program(Project)
+a=int(input())
+b=int(input())
+operator=input()
+if operator == '+':
+    print(a+b)
+elif operator == '-':
+    print(a-b)
+elif operator == '*':
+    print(a*b)
+elif operator == '/':
+    print(a/b)
+elif operator == '**':
+    print(a**b)
